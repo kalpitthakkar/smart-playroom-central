0b6948308d41f9d63ff7a5113bbfdf1a91f031cd
kalpitthakkar <kthakkar1605@gmail.com>
2019-06-01 01:07:47 -0400

    Add all the files

---
+++ kinectpath/source/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+python kinectpath.py --use-grasp-times --plot-traj --plot-traj-trial
+
+# --plot-traj --write-attraction
+
+#    --plot-avg-all
+#    --plot-avg-attract
+#    --plot-avg-curve
+#    --plot-persub-attract
+#    --plot-persub-curve
+
+#    --plot-traj \
+#    --write-attraction
+
+#    --write-plen
+#    --write-props
